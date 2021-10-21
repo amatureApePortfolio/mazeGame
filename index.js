788b62ec5cc1d3269d857e620fe5f11f49553a42
drdarian <dariantaban@gmail.com>
2021-10-20 21:23:19 -0700

    movements built

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
 const cells = 30;
 const width = 600;
@@ -164,16 +164,19 @@ const ball = Bodies.circle(
 World.add(world, ball);
 
 document.addEventListener('keydown', event => {
+  const { x, y } = ball.velocity
+  console.log(x, y)
+
   if (event.keyCode === 87) {
-    console.log('move ball up')
+    Body.setVelocity(ball, { x, y: y - 5 })
   }
   if (event.keyCode === 68) {
-    console.log('move ball right')
+    Body.setVelocity(ball, { x: x + 5, y })
   }
   if (event.keyCode === 83) {
-    console.log('move ball down')
+    Body.setVelocity(ball, { x, y: y + 5 })
   }
   if (event.keyCode === 65) {
-    console.log('move ball left')
+    Body.setVelocity(ball, { x: x - 5, y })
   }
 })
