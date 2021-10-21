2d5319ce53318004a906b6d16c5f122a1a3f36eb
drdarian <dariantaban@gmail.com>
2021-10-20 17:07:10 -0700

    canvas refactoring for dynamic dimensions

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width: width,
+    height: height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+// const shape = Bodies.rectangle(200, 200, 50, 50, {
+//   isStatic: true
+// });
+// World.add(world, shape);
+
+// Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true})
+]
+
+World.add(world, walls);
